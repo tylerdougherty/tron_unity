d8ec984dc0aac5f127f06483d8c46e7b761327af
theandypeterson <arp25@students.calvin.edu>
2015-04-27 22:10:24 -0400

    Added basic player and camera elements
    created a cube for bike. tried to add some rotation via ‘a’ and ‘d’ keys

---
+++ Assets/Scripts/BikeController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BikeController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+	private bool isRotating;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		isRotating = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		bool wPressed = Input.GetKey ("a");
+		bool dPressed = Input.GetKey ("d");
+
+		if (!(wPressed || dPressed))
+			isRotating = false;
+
+		if (wPressed && !isRotating) {
+			isRotating = true;
+			rb.transform.Rotate (0,90,0);
+		}
+		if (dPressed && !isRotating) {
+			isRotating = true;
+			rb.transform.Rotate (0,-90,0);
+		}
+
+		Vector3 movement = new Vector3 (0, 0, 1);
+		rb.AddForce (movement * speed);
+	}
+}
