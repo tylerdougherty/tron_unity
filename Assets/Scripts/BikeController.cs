ade36682d570b94eb993af361c03217dee490d15
theandypeterson <arp25@students.calvin.edu>
2015-05-05 16:22:53 -0400

    My attempts at trail collisions. I'm so close...

--- Assets/Scripts/BikeController.cs
+++ Assets/Scripts/BikeController.cs
@@ -10,11 +10,14 @@ namespace Assets.Scripts
 
 		private Rigidbody _rb;
 		private bool _isRotating;
-		private List<List<Vector3>> _trails;
+		public List<Vector3> _trail;
+		public List<Vector3> _otherTrail;
 		private bool _dead;
 		private float _speed;
 		private Collider _collider;
-		private int playerNumber;
+		private int _playerNumber;
+		private NetworkView _networkView;
+		private Vector3 _myPosition;
 
 
 		// Use this for initialization
@@ -25,15 +28,16 @@ namespace Assets.Scripts
 			_dead = false;
 			_speed = Speed;
 			_collider = GetComponent<Collider>();
+			_networkView = GetComponent<NetworkView> ();
 			
-			_trails = new List<List<Vector3>>();
-			_trails.Add (new List<Vector3> ());
-			_trails.Add (new List<Vector3> ());
+			_trail = new List<Vector3>();
+			_otherTrail = new List<Vector3>();
+			_myPosition = new Vector3 ();
 			//This works for only two players, but it's something...
 			if (Network.isClient)
-				playerNumber = 1;
+				_playerNumber = 1;
 			else
-				playerNumber = 0;
+				_playerNumber = 0;
 		}
 
 		// Update is called once per frame
@@ -51,8 +55,8 @@ namespace Assets.Scripts
 				var dPressed = Input.GetKey ("d");
 				var rPressed = Input.GetKey ("r");
 
-				//if (rPressed && Network.isServer)
-					//Debug.Log ("restart");
+				if (rPressed && Network.isServer)
+					Debug.Log ("restart");
 
 				if (!(aPressed || dPressed))
 					_isRotating = false;
@@ -69,27 +73,51 @@ namespace Assets.Scripts
 				_rb.velocity = transform.forward * _speed;
 
 				// Store trail coordinates
-				_trails[playerNumber].Add (transform.position);
+				_myPosition = transform.position;
+				_trail.Add (transform.position);
+				//Debug.Log ("My Trail Count: " + _trail.Count);
+				_networkView.RPC("getPosition", RPCMode.Others, _myPosition);
+				//Debug.Log ("Other Trail Count: " + _otherTrail.Count);
+
 				//Debug.Log (transform.position);
 				//Debug.Log (_collider.transform.position);
 				//Debug.Log ("---");
 
-				if (Hit ())
-					_dead = true;
+				_networkView.RPC ("CheckHit", RPCMode.All);
 			} else {
 				enabled = false;
 			}
 		}
 
-		private bool Hit()
+		[RPC]
+		void getPosition(Vector3 position){
+			//Debug.Log ("Client: " +position);
+			//Debug.Log (this._playerNumber + ": " + this._otherTrail.Count);
+			this._otherTrail.Add (position);
+		}
+
+		[RPC]
+		private void CheckHit()
 		{
-			for (var i = 0; i < _trails.Count; i ++) {
-				for (var j = 0; j < _trails[i].Count - 1; j++) {
-					if (Physics.Linecast (_trails [i] [j], _trails [i] [j + 1]) && _collider == GetComponent<Collider>())
-						return true;
+			Debug.Log (_playerNumber + ": " + _otherTrail.Count);
+			for (var i = 0; i < _trail.Count - 1; i ++) {
+				RaycastHit rh = new RaycastHit();
+				if (Physics.Linecast (_trail [i], _trail [i+1],out rh)){
+					if(rh.collider == _collider) {
+						Debug.Log ("Hit My Line");
+						_dead = true;
+					}
 				}
 			}
-			return false;
+			for (var i = 0; i < _otherTrail.Count - 1; i ++) {
+				RaycastHit rh = new RaycastHit();
+				if (Physics.Linecast (_otherTrail [i], _otherTrail [i+1],out rh)){
+					if(rh.collider == _collider){
+						Debug.Log ("Hit Other Line");
+						_dead = true;
+					}
+				}
+			}
 		}
 
 //		// Called once per frame-rate frame
--- Assets/Scripts/FloorController.cs
+++ Assets/Scripts/FloorController.cs
@@ -1,22 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
 public class FloorController : MonoBehaviour {
 
-
-	private List<Vector3> _trails;
 	// Use this for initialization
 	void Start () {
-		
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-
-	[RPC] void UpdateTrail() {
-
+	
 	}
 }
