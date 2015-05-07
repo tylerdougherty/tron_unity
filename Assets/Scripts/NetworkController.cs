78659654d4a94b05a5da8bb0a464bf2bd8017fb2
Tyler Dougherty <twd3@students.calvin.edu>
2015-05-06 21:44:51 -0400

    Added better spawning system.
    Added support for 'x' number of players, where x is the number of spawn
    locations.
    Host now starts the game by pressing 'p'.

--- Assets/Scripts/NetworkController.cs
+++ Assets/Scripts/NetworkController.cs
@@ -7,69 +7,118 @@ public class NetworkController : MonoBehaviour {
 	public GameObject playerPrefab;
 	public Transform spawnPoint1;
 	public Transform spawnPoint2;
-	public int maxNumberOfPlayers;
+	public int maxActivePlayers;
 
-	private int currentNumberofPlayers;
+	private int activePlayers;
 	private string _gameName = "CS 352 Tron";
-	private bool refreshing = false;
 	private HostData[] hostData;
 	private float btnX, btnY, btnW, btnH;
+	private NetworkView _networkView;
+
+	private GameObject[] spawns;
+	private List<int> unclaimedSpawns = new List<int>();
+	private GameObject mySpawnPoint;
+	private bool gameStarted = false;
+	private System.Random rand = new System.Random();
 
 	void Start() {
-		currentNumberofPlayers = 0;
+		activePlayers = 0;
 		btnX = (float)(Screen.width * 0.05);
 		btnY = (float)(Screen.width * 0.05);
 		btnW = (float)(Screen.width * 0.1);
 		btnH = (float)(Screen.width * 0.1);
+
+		//Get all the spawn points and generate a 'used' list
+		spawns = GameObject.FindGameObjectsWithTag("Spawn Point");
+		for (var i=0; i<spawns.Length; i++)
+			unclaimedSpawns.Add(i);
+
+		//Load the network view
+		_networkView = GetComponent<NetworkView>();
 	}
 
 	public void startServer() {
 		//Debug.Log ("Starting Server");
-		Network.InitializeServer (maxNumberOfPlayers, 25000, !Network.HavePublicAddress());
+		Network.InitializeServer (maxActivePlayers, 25001, !Network.HavePublicAddress());
 		MasterServer.RegisterHost(_gameName, "Tron Game", "This is in beta");
 	}
 
-	public void refreshHostList() {
+	public void RefreshHostList() {
 		MasterServer.RequestHostList (_gameName);
-		refreshing = true;
 	}
 
 	public void Update() {
-		if (refreshing) {
-			if(MasterServer.PollHostList ().Length > 0) {
-				refreshing = false;
-				hostData = MasterServer.PollHostList();
+		if (Network.isServer)
+		{
+			var start = Input.GetKey("p");
+
+			if (!gameStarted && start)
+			{
+				_networkView.RPC("StartGame", RPCMode.All);
+				gameStarted = true;
 			}
 		}
 	}
 
-	void spawnPlayer(int spawnPoint) {
-		if (spawnPoint == 1) {
-			Network.Instantiate (playerPrefab, spawnPoint1.position, Quaternion.identity, 0);
-		} else if (spawnPoint == 2) {
-			Network.Instantiate (playerPrefab, spawnPoint2.position, Quaternion.identity, 0);
-		}
-		//Debug.Log (currentNumberofPlayers);
+	[RPC]
+	void StartGame()
+	{
+		SpawnPlayer();
+	}
+
+	/// <summary>
+	/// Spawns a player at a random unused spawn point.
+	/// </summary>
+	// Could add overloaded version with specific spawn
+	void SpawnPlayer()
+	{
+		//Add player at spawn point
+		Network.Instantiate(playerPrefab, mySpawnPoint.transform.position, Quaternion.identity, 0);
+
+		//Debug.Log (activePlayers);
+	}
+
+	void GetSpawnPoint()
+	{
+		//Get random spawn point
+		var i = unclaimedSpawns[rand.Next(unclaimedSpawns.Count)];
+		mySpawnPoint = spawns[i];
+
+		//Tell everyone the player spawned in
+		_networkView.RPC("ClaimedSpawn", RPCMode.AllBuffered, i);
+	}
+
+	[RPC]
+	void ClaimedSpawn(int spawnPointIndex)
+	{
+		unclaimedSpawns.Remove(spawnPointIndex);
 	}
 
 	void OnServerInitialized() {
 		//Debug.Log ("Server initialized");
-		currentNumberofPlayers++;
-		spawnPlayer(1);
-
+		activePlayers++;
+		Invoke("GetSpawnPoint", 1);
+		//SpawnPlayer();
 	}
 
 	void OnConnectedToServer() {
-		if (currentNumberofPlayers < maxNumberOfPlayers) {
-			currentNumberofPlayers++;
-			spawnPlayer (2);
-
+		if (activePlayers < maxActivePlayers) {
+			activePlayers++;
+			Invoke("GetSpawnPoint", 1);
+			//SpawnPlayer();
 		}
 	}
 
-	void OnMasterServerEvent(MasterServerEvent mse) {
-		if (mse == MasterServerEvent.RegistrationSucceeded) {
-			//Debug.Log ("Registered Server");
+	void OnMasterServerEvent(MasterServerEvent mse)
+	{
+		switch (mse)
+		{
+			case MasterServerEvent.RegistrationSucceeded:
+				//Debug.Log ("Registered Server");
+				break;
+			case MasterServerEvent.HostListReceived:
+				hostData = MasterServer.PollHostList();
+				break;
 		}
 	}
 
@@ -79,12 +128,12 @@ public class NetworkController : MonoBehaviour {
 				startServer ();
 			}
 			if (GUI.Button (new Rect (btnX, (float)(btnY * 1.2 + btnH), btnW, btnH), "Refresh")) {
-				refreshHostList ();
+				RefreshHostList ();
 			}
 			if (hostData != null) {
 				for (int i = 0; i < hostData.Length; i++) {
 					if(GUI.Button (new Rect ((float)(btnX * 1.5 + btnW), (float)(btnY * 1.2 + (btnH * i)), (float)(btnW * 3), (float)(btnH * 0.5)), hostData [i].gameName)) {
-						Network.Connect(hostData[i]);
+						Debug.Log(Network.Connect(hostData[i]));
 					}
 				}
 			}
